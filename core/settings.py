e02924afe76c34851a3c40f3a10d273d3d877ae7
zakhep66 <sur.mikhael@gmail.com>
2023-04-08 23:33:48 +0300

    add refresh token for token/refresh/ endpoint

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": False,
+    "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
 
