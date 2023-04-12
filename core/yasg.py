3e3bd5dc9d57bdc6b00efb922a6f0374d695373f
zakhep66 <sur.mikhael@gmail.com>
2023-04-12 10:13:15 +0300

    add repath api/v1/swagger

--- core/yasg.py
+++ core/yasg.py
@@ -41,6 +41,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
