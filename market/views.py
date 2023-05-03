a672c59bb1f433fa8ce1e3a4db48aed020e713e6
zakhep66 <sur.mikhael@gmail.com>
2023-05-03 22:52:59 +0300

    Новое назвÐadd method doc for market view

--- market/views.py
+++ market/views.py
@@ -36,6 +36,9 @@ class StoreProductViewSet(viewsets.ReadOnlyModelViewSet):
 
     @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated, ])
     def all_student_product(self, request):
+        """
+        Возвращает все товары купленные студентом
+        """
         return Response(
             *MarketHandler.get_all_student_buy(request.user.id)
         )
@@ -53,17 +56,27 @@ class StoreHistoryViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['post'], permission_classes=[IsStudent, ])
     def market_shop(self, request):
+        """
+        Совершает покупку товара
+        """
         return Response(
             *MarketHandler.make_shop(student_id=request.user.id, product_id=request.data.get('product_id'))
         )
 
     @action(detail=False, methods=['get'], permisstion_classes=[IsAuthenticated, ])
     def all_non_issued_items(self, request):
+        """
+        Возвращает все не выданные товары типа merch
+        """
         return Response(
             *MarketHandler.get_all_non_issued_items()
         )
 
     def partial_update(self, request, *args, **kwargs):
+        """
+        Изменяет статус выдачи товара типа merch на True
+        принимает значение 'status' в теле запроса
+        """
         store_history = self.get_object()
 
         if 'status' not in request.data:
