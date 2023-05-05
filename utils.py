124db44a5ca98948ed34c3fad9f12b71eac9ef47
zakhep66 <sur.mikhael@gmail.com>
2023-05-06 00:05:07 +0300

    add upload_to function for achievement

--- utils.py
+++ utils.py
@@ -3,60 +3,75 @@ import uuid
 
 
 def get_upload_path_for_icon(instance, filename):
-    """
-    Генерирует уникальное имя файла и возвращает путь для сохранения.
-    """
-    ext = filename.split('.')[-1].lower()
-    if ext not in ['jpg', 'png', 'jpeg', 'svg']:
-        raise ValueError('Недопустимое расширение файла. Разрешены только jpg, jpeg, png, svg.')
-    filename = f"{uuid.uuid4().hex}.{ext}"
-    path = os.path.join(
-        'icons',  # имя папки для сохранения файлов
-        filename  # имя файла
-    )
-    return path
+	"""
+	Генерирует уникальное имя файла и возвращает путь для сохранения.
+	"""
+	ext = filename.split('.')[-1].lower()
+	if ext not in ['jpg', 'png', 'jpeg', 'svg']:
+		raise ValueError('Недопустимое расширение файла. Разрешены только jpg, jpeg, png, svg.')
+	filename = f"{uuid.uuid4().hex}.{ext}"
+	path = os.path.join(
+		'icons',  # имя папки для сохранения файлов
+		filename  # имя файла
+	)
+	return path
 
 
 def get_upload_path_for_users(instance, filename):
-    """
-    Генерирует уникальное имя файла для аватарки и возвращает путь для сохранения.
-    """
-    ext = filename.split('.')[-1].lower()
-    if ext not in ['jpg', 'png', 'jpeg', 'svg']:
-        raise ValueError('Недопустимое расширение файла. Разрешены только jpg, jpeg, png, svg.')
-    filename = f"{uuid.uuid4().hex}.{ext}"
-    path = os.path.join(
-        'users',  # имя папки для сохранения файлов
-        filename  # имя файла
-    )
-    return path
+	"""
+	Генерирует уникальное имя файла для аватарки и возвращает путь для сохранения.
+	"""
+	ext = filename.split('.')[-1].lower()
+	if ext not in ['jpg', 'png', 'jpeg', 'svg']:
+		raise ValueError('Недопустимое расширение файла. Разрешены только jpg, jpeg, png, svg.')
+	filename = f"{uuid.uuid4().hex}.{ext}"
+	path = os.path.join(
+		'users',  # имя папки для сохранения файлов
+		filename  # имя файла
+	)
+	return path
 
 
 def get_upload_path_for_market_image(instance, filename):
-    """
-    Генерирует уникальное имя файла для продукта и возвращает путь для сохранения.
-    """
-    ext = filename.split('.')[-1].lower()
-    if ext not in ['jpg', 'png', 'jpeg', 'svg']:
-        raise ValueError('Недопустимое расширение файла. Разрешены только jpg, jpeg, png, svg.')
-    filename = f"{uuid.uuid4().hex}.{ext}"
-    path = os.path.join(
-        'product',  # имя папки для сохранения файлов
-        filename  # имя файла
-    )
-    return path
+	"""
+	Генерирует уникальное имя файла для продукта и возвращает путь для сохранения.
+	"""
+	ext = filename.split('.')[-1].lower()
+	if ext not in ['jpg', 'png', 'jpeg', 'svg']:
+		raise ValueError('Недопустимое расширение файла. Разрешены только jpg, jpeg, png, svg.')
+	filename = f"{uuid.uuid4().hex}.{ext}"
+	path = os.path.join(
+		'product',  # имя папки для сохранения файлов
+		filename  # имя файла
+	)
+	return path
 
 
 def get_upload_path_for_emoji(instance, filename):
-    """
-    Генерирует уникальное имя файла для продукта и возвращает путь для сохранения.
-    """
-    ext = filename.split('.')[-1].lower()
-    if ext not in ['jpg', 'png', 'jpeg', 'svg']:
-        raise ValueError('Недопустимое расширение файла. Разрешены только jpg, jpeg, png, svg.')
-    filename = f"{uuid.uuid4().hex}.{ext}"
-    path = os.path.join(
-        'emoji',  # имя папки для сохранения файлов
-        filename  # имя файла
-    )
-    return path
+	"""
+	Генерирует уникальное имя файла для продукта и возвращает путь для сохранения.
+	"""
+	ext = filename.split('.')[-1].lower()
+	if ext not in ['jpg', 'png', 'jpeg', 'svg']:
+		raise ValueError('Недопустимое расширение файла. Разрешены только jpg, jpeg, png, svg.')
+	filename = f"{uuid.uuid4().hex}.{ext}"
+	path = os.path.join(
+		'emoji',  # имя папки для сохранения файлов
+		filename  # имя файла
+	)
+	return path
+
+
+def get_upload_path_for_achievement(instance, filename):
+	"""
+	Генерирует уникальное имя файла для ачивки и возвращает путь для сохранения.
+	"""
+	ext = filename.split('.')[-1].lower()
+	if ext not in ['jpg', 'png', 'jpeg', 'svg']:
+		raise ValueError('Недопустимое расширение файла. Разрешены только jpg, jpeg, png, svg.')
+	filename = f"{uuid.uuid4().hex}.{ext}"
+	path = os.path.join(
+		'achievement',  # имя папки для сохранения файлов
+		filename  # имя файла
+	)
+	return path
