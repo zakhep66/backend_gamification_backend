1300e54b2a61caa7efb50db54354fc669e979866
zakhep66 <sur.mikhael@gmail.com>
2023-05-07 05:09:05 +0300

    add method for get_available_products_for_student

--- market/models.py
+++ market/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import utils
 
-from users.models import BankAccount
+from users.models import BankAccount, Student
 
 
 class StoreHistory(models.Model):
@@ -21,11 +21,17 @@ class StoreHistory(models.Model):
 
 
 class StoreProduct(models.Model):
+    PRODUCT_TYPE = (
+        ('merch', 'merch'),
+        ('back_color', 'back_color'),
+        ('emoji', 'emoji')
+    )
+
     name = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     image = models.ImageField(upload_to=utils.get_upload_path_for_market_image)
     price = models.PositiveIntegerField(null=False, blank=False)
-    product_type = models.CharField(max_length=50)
+    product_type = models.CharField(max_length=50, choices=PRODUCT_TYPE)
 
     class Meta:
         verbose_name = 'Товар'
@@ -33,3 +39,11 @@ class StoreProduct(models.Model):
 
     def __str__(self):
         return self.name
+
+    @staticmethod
+    def get_available_products_for_student(student: Student):
+        bought_products = StoreHistory.objects.filter(
+            buyer_bank_account_id=student.bank_account_id.id,
+        ).values_list('store_product_id', flat=True)
+        available_products = StoreProduct.objects.exclude(product_type='merch').exclude(id__in=bought_products)
+        return available_products
