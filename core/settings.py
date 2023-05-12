9936d3bd8246509b5cb91c74a0cf6fab29ba501d
zakhep66 <sur.mikhael@gmail.com>
2023-05-12 22:51:30 +0300

    removed the complexity of the password

--- core/settings.py
+++ core/settings.py
@@ -206,7 +206,7 @@ SIMPLE_JWT = {
 }
 
 # redis related settings
-REDIS_HOST = "localhost"
+REDIS_HOST = "redis"
 REDIS_PORT = "6379"
 
 CELERY_BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
@@ -215,3 +215,5 @@ CELERY_RESULT_BACKEND = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
+
+AUTH_PASSWORD_VALIDATORS = []
