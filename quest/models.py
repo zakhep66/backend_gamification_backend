57d001cecbaad83ec096c8430ec033e819742b03
zakhep66 <sur.mikhael@gmail.com>
2023-05-23 05:00:34 +0300

    add min max column in quest type

--- quest/models.py
+++ quest/models.py
@@ -24,6 +24,8 @@ class Quest(models.Model):
 class QuestType(models.Model):
 	name = models.CharField(max_length=50)
 	color = models.CharField(max_length=20)
+	min_sum = models.PositiveIntegerField()
+	max_sum = models.PositiveIntegerField()
 
 	def __str__(self):
 		return self.name
